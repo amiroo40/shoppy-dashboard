2b93f6e41d94a627eae94e8abcfbd0a6d487445f
amirreza <amirreza.s.1414@gmail.com>
2024-12-03 17:53:30 +0330

    theme settings completed

--- src/App.js
+++ src/App.js
@@ -4,20 +4,25 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 import { FiSettings } from 'react-icons/fi'
 import './App.css'
 import { Area, Bar, Calendar, ColorMapping, ColorPicker, Customers, ECommerce, Editor, Employees, Financial, Kanban, Line, Orders, Pie, Pyramid, Stacked } from './pages'
-import { Navbar, Sidebar } from './components'
+import { Navbar, Sidebar, ThemeSettings } from './components'
 
 import { useStateContext } from './context/ContextProvider'
 
 const App = () => {
-    const { activeMenu } = useStateContext()
+    const { activeMenu, themeSettings, setThemeSettings, currentColor, currentMode } = useStateContext()
 
     return (
-        <div>
+        <div className={currentMode === 'Dark' ? 'dark' : ''}>
             <BrowserRouter>
                 <div className="flex relative dark:bg-main-dark-bg">
                     <div className="fixed bottom-4 right-4" style={{ zIndex: '1000' }}>
                         <TooltipComponent content='Settings' position='Top'>
-                            <button type='button' className='text-3xl hover:drop-shadow-xl hover:bg-light-gray p-3 text-white' style={{ background: 'blue', borderRadius: '50%' }}>
+                            <button
+                                type='button'
+                                className='text-3xl hover:drop-shadow-xl hover:bg-light-gray p-3 text-white'
+                                style={{ background: currentColor, borderRadius: '50%' }}
+                                onClick={() => setThemeSettings(true)}
+                            >
                                 <FiSettings />
                             </button>
                         </TooltipComponent>
@@ -32,11 +37,18 @@ const App = () => {
                                 <Sidebar />
                             </div>
                         )}
-                    <div className={`w-full dark:bg-main-bg bg-main-bg min-h-screen ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
+                    <div className={
+                        `w-full dark:bg-main-dark-bg bg-main-bg min-h-screen 
+                        ${activeMenu
+                            ? 'md:ml-72' : 'flex-2'}`
+                    }>
                         <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg w-full navbar">
                             <Navbar />
                         </div>
+
                         <div>
+                            {themeSettings && <ThemeSettings />}
+
                             <Routes>
                                 {/* Dashboard */}
                                 <Route path='/' element={<ECommerce />} />
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ import { links } from '../data/dummy'
 import { useStateContext } from '../context/ContextProvider'
 
 const Sidebar = () => {
-    const { activeMenu, setActiveMenu, screenSize } = useStateContext()
+    const { activeMenu, setActiveMenu, screenSize, currentColor } = useStateContext()
 
     const handleCloseSideBar = () => {
         if (activeMenu && screenSize <= 900) {
@@ -50,6 +50,11 @@ const Sidebar = () => {
                                         to={`/${link.name}`}
                                         key={link.name}
                                         onClick={handleCloseSideBar}
+
+                                        style={({ isActive }) => ({
+                                            backgroundColor: isActive ? currentColor : ''
+                                        })}
+
                                         className={({ isActive }) => isActive ? activeLink : normalLink}
                                     >
                                         {link.icon}
