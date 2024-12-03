8739759cfec38818052906fa42b80b17c73c3734
amirreza <amirreza.s.1414@gmail.com>
2024-12-03 15:52:01 +0330

    calendar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
     }, [setScreenSize])
 
     useEffect(() => {
-        if (screenSize >= 900) {
+        if (screenSize <= 900) {
             setActiveMenu(false);
         } else {
             setActiveMenu(true)
